283a9efb3867c2349e8269606b591b7d1e822a23
Wouter Hobers <wouter.hobers+github@gmail.com>
2021-10-01 20:56:12 +0200

    Experiment with lower load address.

--- foo.asm
+++ foo.asm
@@ -1,13 +1,13 @@
   ;; To execute from BASIC:
   ;;
-  ;; 10 CLEAR 32767
-  ;; 20 LOAD "foo.bin" CODE 32768
-  ;; 30 RANDOMIZE USR 32768
+  ;; 10 CLEAR %$5FFF
+  ;; 20 LOAD "foo.bin" CODE %$6000
+  ;; 30 RANDOMIZE % USR $6000
   ;; 40 PAUSE 0
 
   device zxspectrumnext
 
-  org $8000
+  org $6000
 
 start:
   ld hl,message
@@ -20,4 +20,4 @@ loop:
   jp loop
 
 message:
-  db 'Hello, world!',$0d,0
+  db '??? Hello, world!',$0d,0
