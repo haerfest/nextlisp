918e230f25dc4031bc1c5ae7d34e562f9259e4e3
Wouter Hobers <wouter.hobers+github@gmail.com>
2021-10-03 15:11:48 +0200

    Tokenize strings correctly.

--- lisp.c
+++ lisp.c
@@ -3,23 +3,8 @@
 #include <string.h>
 
 
-int is_eos(char s) {
-  return s == '\0';
-}
-
-
-int is_whitespace(char s) {
-  return (s == ' ' || s == '\t');
-}
-
-
-int is_bracket(char s) {
-  return (s == '(' || s == ')');
-}
-
-
 char* skip_whitespace(char* s) {
-  while (!is_eos(*s) && is_whitespace(*s)) {
+  while (*s == ' ' || *s == '\t' || *s == '\n') {
     s++;
   }
 
@@ -35,92 +20,139 @@ typedef enum {
 
 
 typedef struct token_t {
-  struct token_t* next;
-  token_type_t    type;
-  char*           value;
+  token_type_t type;
+  char*        value;
 } token_t;
 
 
-token_t* add_token(token_t* tokens, token_type_t type, char* value) {
-  token_t* token;
+typedef struct {
+  size_t   count;
+  token_t* tokens;
+} tokens_t;
 
-  token = malloc(sizeof(token_t));
-  if (token == NULL) {
-    perror(NULL);
-    exit(1);
-  }
 
-  token->next  = NULL;
-  token->type  = type;
-  token->value = value;
-
-  if (tokens == NULL) {
-    return token;
-  }
-
-  token_t* last = tokens;
-  while (last->next) {
-    last = last->next;
-  }
-  last->next = token;
-
-  return tokens;
+void error(char* message) {
+  fprintf(stderr, "error: %s\n", message);
 }
 
 
-token_t* tokenize(char* s) {
-  token_t* tokens = NULL;
+void fatal(char* message) {
+  error(message);
+  exit(1);
+}
 
+
+void add_token(tokens_t* tokens, token_type_t type, char* value) {
+  token_t* reallocated = realloc(tokens->tokens, sizeof(token_t) * (tokens->count + 1));
+  if (reallocated == NULL) {
+    fatal("out of memory");
+  }
+
+  tokens->tokens                      = reallocated;
+  tokens->tokens[tokens->count].type  = type;
+  tokens->tokens[tokens->count].value = value;
+  tokens->count++;
+}
+
+
+void free_tokens(tokens_t* tokens) {
+  free(tokens->tokens);
+  tokens->tokens = NULL;
+  tokens->count  = 0;
+}
+
+
+void tokenize(char* s, tokens_t* tokens) {
   for (;;) {
     s = skip_whitespace(s);
-    if (is_eos(*s)) {
-      break;
+    if (*s == '\0') {
+      return;
     }
 
-    if (*s == '(') {
-      tokens = add_token(tokens, TOKEN_LIST_BEGIN, NULL);
-      s++;
-    } else if (*s == ')') {
-      add_token(tokens, TOKEN_LIST_END, NULL);
-      s++;
-    } else {
-      char* start = s;
-      while (!is_eos(*s) && !is_whitespace(*s) && !is_bracket(*s)) {
+    switch (*s) {
+      case '(':
+        add_token(tokens, TOKEN_LIST_BEGIN, NULL);
         s++;
+        break;
+
+      case ')':
+        add_token(tokens, TOKEN_LIST_END, NULL);
+        s++;
+        break;
+
+      case '"':
+      {
+        char* start      = s;
+        int   is_escaped = 0;
+
+        s++;
+        while (*s != '\0' && (*s != '"' || is_escaped)) {
+          if (is_escaped) {
+            is_escaped = 0;
+          } else if (*s == '\\') {
+            is_escaped = 1;
+          }
+          s++;
+        }
+
+        if (*s == '\0') {
+          error("unterminated string");
+          free_tokens(tokens);
+          return;
+        }
+
+        s++;
+
+        size_t n     = s - start;
+        char*  value = strndup(start, n);
+        if (value == NULL) {
+          fatal("out of memory");
+        }
+        add_token(tokens, TOKEN_ATOM, value);
+        break;
       }
-      size_t n = s - start;
-      char* atom = malloc(n + 1);
-      if (atom == NULL) {
-        perror(NULL);
-        exit(1);
+
+      default:
+      {
+        char* start = s;
+        while (*s != '\0' && *s != '(' && *s != ')' && *s != ' ' && *s != '\t' && *s != '\n') {
+          s++;
+        }
+
+        size_t n     = s - start;
+        char*  value = strndup(start, n);
+        if (value == NULL) {
+          fatal("out of memory");
+        }
+        add_token(tokens, TOKEN_ATOM, value);
+        break;
       }
-      strncpy(atom, start, n);
-      atom[n] = '\0';
-      tokens = add_token(tokens, TOKEN_ATOM, atom);
     }
   }
-
-  return tokens;
 }
 
 
-void print_tokens(token_t* tokens) {
-  while (tokens) {
-    switch (tokens->type) {
+void print_tokens(tokens_t* tokens) {
+  size_t i;
+
+  printf("%lu tokens\n", tokens->count);
+
+  for (i = 0; i < tokens->count; i++) {
+    token_t* token = &tokens->tokens[i];
+
+    switch (token->type) {
       case TOKEN_LIST_BEGIN:
-        printf("LIST_BEGIN ");
+        printf("LIST( ");
         break;
 
       case TOKEN_LIST_END:
-        printf("LIST_END ");
+        printf(")LIST ");
         break;
 
       case TOKEN_ATOM:
-        printf("ATOM:'%s' ", tokens->value);
+        printf("ATOM(%s) ", token->value);
         break;
     }
-
-    tokens = tokens->next;
   }
 
   printf("\n");
@@ -139,17 +171,17 @@ typedef int expr_t;
 
 
 expr_t read(FILE* in) {
-  char     buffer[128 + 1];
-  token_t* tokens;
+  char buffer[128 + 1];
 
   (void) fgets(buffer, sizeof(buffer), in);
   if (feof(in)) {
     exit(0);
   }
 
-  chomp(buffer);
-  tokens = tokenize(buffer);
-  print_tokens(tokens);
+  tokens_t tokens = {0, NULL};
+  tokenize(buffer, &tokens);
+  print_tokens(&tokens);
+  free_tokens(&tokens);
 
   return 0;
 }
