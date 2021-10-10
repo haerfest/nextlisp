74133afbfded51de5843746ec158c4fe908c8169
Wouter Hobers <wouter.hobers+github@gmail.com>
2021-10-10 20:49:26 +0200

    Initialise tilemap mode.

--- lisp.asm
+++ lisp.asm
@@ -1,124 +1,95 @@
+  ;; To assemble:
+  ;;
+  ;;   $ sjasmplus --raw=lisp.bin lisp.asm
+  ;; 
   ;; To execute from BASIC:
   ;;
-  ;; 10 CLEAR %$5FFF
-  ;; 20 LOAD "foo.bin" CODE %$6000
-  ;; 30 RANDOMIZE % USR $6000
-  ;; 40 PAUSE 0
+  ;;   10 CLEAR %$5FFF
+  ;;   20 LOAD "lisp.bin" CODE %$6000
+  ;;   30 RANDOMIZE % USR $6000
+
+ROM_CHARACTER_SET            equ $3D00
+TILEMAP_ADDR                 equ $4000 + 1024
+
+NEXTREG_TILEMAP_CLIP         equ $1B
+NEXTREG_TILEMAP_OFFSET_X_MSB equ $2F
+NEXTREG_TILEMAP_OFFSET_X     equ $30
+NEXTREG_TILEMAP_OFFSET_Y     equ $31
+NEXTREG_PALETTE_INDEX        equ $40
+NEXTREG_PALETTE_VALUE        equ $41
+NEXTREG_ENHANCED_ULA_CTRL    equ $43
+NEXTREG_ULA_CTRL             equ $68
+NEXTREG_TILEMAP_CONTROL      equ $6B
+NEXTREG_TILEMAP_DEFAULT_ATTR equ $6C
+NEXTREG_TILEMAP_MAP_ADDR     equ $6E 
+NEXTREG_TILEMAP_DEF_ADDR     equ $6F
 
   device zxspectrumnext
 
-  ;; https://skoolkid.github.io/rom/index.html
-ROM_CLS            equ $0DAF
-  
-INPUT_SIZE_MAX         equ 30
-ATTRIBUTE_FILE         equ $5800
-AT                     equ $16
-BLUE_ON_WHITE_BLINKING equ %11111001
-BLACK_ON_GRAY          equ %00111000
-CR                     equ $0D
-  
   org $6000
 
-start:
+main:
   call init
-.loop:
-  call read
-  call eval
-  call print
+  call print_banner
   jp   $
-  jr   .loop
+
 
 init:
-  call ROM_CLS
-  call header_print
+  call init_tilemap
   ret
 
-read:
-  call prompt_print
-  call cursor_on
+init_tilemap:
+  nextreg NEXTREG_ULA_CTRL,%10000000
+  nextreg NEXTREG_TILEMAP_DEF_ADDR,     $00 ; $4000
+  nextreg NEXTREG_TILEMAP_MAP_ADDR,     (TILEMAP_ADDR - $4000) >> 8
+  nextreg NEXTREG_TILEMAP_DEFAULT_ATTR, %00000000
+  nextreg NEXTREG_TILEMAP_CLIP,         0
+  nextreg NEXTREG_TILEMAP_CLIP,         159
+  nextreg NEXTREG_TILEMAP_CLIP,         0
+  nextreg NEXTREG_TILEMAP_CLIP,         255
+  nextreg NEXTREG_TILEMAP_OFFSET_X_MSB, 0
+  nextreg NEXTREG_TILEMAP_OFFSET_X,     0
+  nextreg NEXTREG_TILEMAP_OFFSET_Y,     0
+  nextreg NEXTREG_TILEMAP_CONTROL,      %10101001
+
+  ;; Select tilemap palette.
+  nextreg NEXTREG_ENHANCED_ULA_CTRL, %00110000
+  nextreg NEXTREG_PALETTE_INDEX,     0
+  nextreg NEXTREG_PALETTE_VALUE,     %00000000 ; #0 = black
+  nextreg NEXTREG_PALETTE_VALUE,     %11111111 ; #1 = white
+
+  ;; Copy the ROM character set, which starts at character 32 (space).
+  ;; The 128 characters take up 128 x 8 = 1024 = $400 bytes.
+  ld  hl,ROM_CHARACTER_SET
+  ld  de,$4000 + 32 * 8
+  ld  bc,$4000 - $3D00
+  ldir
+
   ret
 
-eval:
-  ret
+print_hl_at_bc:
+  ;; Make DE point to TILEMAP_ADDR[B][C]. 
+  ld  d,b
+  ld  e,32
+  mul de
+  ld  a,c
+  add de,a
+  add de,TILEMAP_ADDR
 
-print:
-  ret
-
-header_print:
-  ld   hl,header
-  jp   hl_print
-
-prompt_print:
-  ld   hl,prompt
-  call hl_print
-  ret
-
-hl_print: 
-  ld   a,(hl)
-  or   a
-  ret  z
-  call a_print
-  inc  hl
-  jr   hl_print
-
-a_print:
-  push af
-  rst  $10
-  pop  af
-  cp   CR
-  jp   z,cursor_cr
-  jp   cursor_advance
-
-cursor_cr:
-  xor a
-  ld  (cursor_x),a
-  ld  a,(cursor_y)
-  cp  23
+  ;; Print each character until zero byte.
+.print:
+  ld  a,(hl)
+  or  a
   ret z
-  inc a
-  ld  (cursor_y),a
-  ret
+  ld  (de),a
+  inc hl
+  inc de
+  jr  .print
+  
+print_banner:
+  ld  hl,banner
+  ld  bc,0
+  jp  print_hl_at_bc
 
-cursor_advance:
-  ld   a,(cursor_x)
-  cp   31
-  jp   z,cursor_cr
-  inc  a
-  ld   (cursor_x),a
-  ret
-
-cursor_on:
-  call cursor_attr_location_get
-  ld   a,BLUE_ON_WHITE_BLINKING
-  ld   (de),a
-  ret
-
-cursor_off:
-  call cursor_attr_location_get
-  ld   a,BLACK_ON_GRAY
-  ld   (de),a
-  ret
-
-cursor_attr_location_get:
-  ld   a,(cursor_y)
-  ld   d,a
-  ld   e,32
-  mul  d,e
-  ld   a,(cursor_x)
-  add  de,a
-  add  de,ATTRIBUTE_FILE
-  ret
-
-header:
-  db   'NextLISP',CR,'Built ',__DATE__,' ', __TIME__,CR,0
-prompt:
-  db   CR,'>',0
-cursor_y:
-  db    0
-cursor_x:
-  db    0
-input_size:
-  db    0
-input:
-  block INPUT_SIZE_MAX,0
-  db    0
+banner:
+  db "NextLISP (Built ", __DATE__, " ", __TIME__, ")",0
